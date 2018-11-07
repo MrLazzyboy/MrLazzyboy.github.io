6f34772fae36fc8b9ec8ce01e5a14af026c0301f
MrLazzyboy <msrlazzyboy@gmail.com>
2018-11-07 20:23:16 +0300

    Go to the web!

---
+++ precache-manifest.2f33037c13835adda78fba75650b0dad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "69aa6305cc839f06a469",
+    "url": "/static/js/main.69aa6305.chunk.js"
+  },
+  {
+    "revision": "cb00d202dd7d7c99ac35",
+    "url": "/static/js/1.cb00d202.chunk.js"
+  },
+  {
+    "revision": "69aa6305cc839f06a469",
+    "url": "/static/css/main.7986e38d.chunk.css"
+  },
+  {
+    "revision": "cb00d202dd7d7c99ac35",
+    "url": "/static/css/1.8cee205f.chunk.css"
+  },
+  {
+    "revision": "bfc6d6a124dac44dcae545395f59b8dc",
+    "url": "/index.html"
+  }
+];
